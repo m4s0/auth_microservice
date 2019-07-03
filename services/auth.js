81d862a6bdd8fa33c6f51d9e2e4bcb769073c845
m4s0 <neoomy@gmail.com>
2019-07-03 15:34:10 +0200

    adds auth logic and tests

---
+++ services/auth.js
@@ -0,0 +1,7 @@
+
+'use strict'
+
+const AuthMongoJwt = require('@matteo.collina/fastify-auth-mongo-jwt')
+
+// needs to be defined with fastify-plugin if we are wrapping it
+module.exports = AuthMongoJwt
